5fcac3f7f48a02a2326a9f3adeee90f918983c8d
Daniel Vrcic <daniel.vrcic@gmail.com>
2017-02-19 18:23:48 +0100

    Message extended with all possible attributes

--- amsmsg.py
+++ amsmsg.py
@@ -2,9 +2,12 @@ import json
 from base64 import b64encode, b64decode
 
 class AmsMessage(object):
-    def __init__(self, *args, **kwargs):
-        self.attributes = kwargs['attributes']
-        self.data = b64encode(kwargs['data'])
+    def __init__(self, attributes='', data='',
+                 messageId='', publishTime=''):
+        self.attributes = attributes
+        self.data = data
+        self.messageId = messageId
+        self.publishTime = publishTime
 
     def set_attr(self, key, value):
         self.attributes.update({key: value})
@@ -13,8 +16,12 @@ class AmsMessage(object):
         self.data = b64encode(data)
 
     def dict(self):
-        return {'attributes': self.attributes,
-                'data': self.data}
+        d = dict()
+        for attr in ['attributes', 'data', 'messageId', 'publishTime']:
+            v = eval('self.{0}'.format(attr))
+            if v:
+                d.update({attr: v})
+        return d
 
     def get_data(self):
         return b64decode(self.data)
