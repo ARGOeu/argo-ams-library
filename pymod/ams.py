aba716b31ae96568e36d222849d21edc060a3348
Daniel Vrcic <daniel.vrcic@gmail.com>
2018-04-24 16:17:31 +0200

    Refactored error handling with error routes

--- pymod/ams.py
+++ pymod/ams.py
@@ -34,10 +34,15 @@ class AmsHttpRequests(object):
                        "sub_mod_offset": ["post", "https://{0}/v1/projects/{2}/subscriptions/{3}:modifyOffset?key={1}"]}
         # HTTP error status codes returned by AMS according to:
         # http://argoeu.github.io/messaging/v1/api_errors/
-        self.errors = {'post': set([400, 401, 403, 408, 413]),
-                       'get': set([401, 403, 404]),
-                       'put': set([400, 403, 409])
-                       }
+        self.errors_route = {"topic_create": ["put", set([409, 401, 403])],
+                             "sub_create": ["put", set([400, 409, 408, 401, 403])],
+                             "sub_ack": ["post", set([408, 400, 401, 403])],
+                             "topic_get": ["get", set([404, 401, 403])],
+                             "topic_modifyacl": ["post", set([400, 401, 403])],
+                             "sub_get": ["get", set([404, 401, 403])],
+                             "topic_publish": ["post", set([413, 401, 403])],
+                             "sub_pushconfig": ["post", set([400, 401, 403])],
+                             "sub_pull": ["post", set([400, 401, 403])]}
 
     def _make_request(self, url, body=None, route_name=None, **reqkwargs):
         """Common method for PUT, GET, POST HTTP requests with appropriate
@@ -56,13 +61,13 @@ class AmsHttpRequests(object):
                 decoded = json.loads(r.content) if r.content else {}
 
             # JSON error returned by AMS
-            elif r.status_code != 200 and r.status_code in self.errors[m]:
+            elif r.status_code != 200 and r.status_code in self.errors_route[route_name][1]:
                 decoded = json.loads(r.content) if r.content else {}
                 raise AmsServiceException(json=decoded, request=route_name)
 
             # handle other erroneous behaviour and construct error message
             # from plaintxt content in response
-            elif r.status_code != 200 and r.status_code not in self.errors[m]:
+            elif r.status_code != 200 and r.status_code not in self.errors_route[route_name][1]:
                 errormsg = {'error': {'code': r.status_code,
                                       'message': r.content }}
                 raise AmsServiceException(json=errormsg, request=route_name)
