6da36f6be881f341803bedf21130379e428e75de
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-12-18 11:46:27 +0100

    Remove encoding parameter as build is initiated with Py2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import glob
 NAME='argo-ams-library'
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 def get_ver():
